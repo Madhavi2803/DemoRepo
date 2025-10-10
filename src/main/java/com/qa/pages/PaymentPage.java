156e157fdee335319e3479609e7ee9fd4328b33d
Madhavi2803 <madhavi.shantaram-kadam@capgemini.com>
2025-10-10 12:05:42 +0530

    Update PaymentPage.java

--- src/main/java/com/qa/pages/PaymentPage.java
+++ src/main/java/com/qa/pages/PaymentPage.java
@@ -4,6 +4,14 @@ public class PaymentPage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int salary;
+		String name;
+
+		public PaymentPage(int salary,String name){
+			this.salary=salary;
+			this.name=name;
+			
+		}
 
 	}
 
